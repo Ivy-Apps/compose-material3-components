07a01ec500fa0d045a668e952e2042cf8a692bc3
Nicole <nicole.georgievva@gmail.com>
2023-05-25 18:03:52 +0300

    Add Filled Card

---
+++ app/src/main/java/com/ivyapps/material3showcase/component/cards/FilledCard.kt
@@ -0,0 +1,97 @@
+package com.ivyapps.material3showcase.component.cards
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.airbnb.android.showkase.annotation.ShowkaseComposable
+import com.ivyapps.material3showcase.component.ComponentPreview
+import com.ivyapps.material3showcase.component.ShowcasePreview
+
+private const val width = 420
+private const val height = 220
+
+@ComponentPreview
+@ShowkaseComposable(
+    name = "Default",
+    group = "Filled Card",
+    widthDp = width,
+    heightDp = height,
+)
+@Composable
+fun FilledCardDefault() {
+    ShowcasePreview(width = width, height = height) {
+        Card {
+            Text(
+                modifier = Modifier.padding(8.dp),
+                text = "Title",
+                style = MaterialTheme.typography.headlineMedium
+            )
+
+            Text(
+                modifier = Modifier.padding(8.dp),
+                text = "From your recent favorites",
+                style = MaterialTheme.typography.bodySmall
+            )
+
+            Button(
+                modifier = Modifier.padding(8.dp),
+                onClick = {
+                    /* Do something! */
+                }
+            ) {
+                Text("Buy")
+            }
+        }
+    }
+}
+
+@ComponentPreview
+@ShowkaseComposable(
+    name = "Custom",
+    group = "Filled Card",
+    widthDp = width,
+    heightDp = height,
+)
+@Composable
+fun FilledCardCustom() {
+    ShowcasePreview(width = width, height = height) {
+        Card(
+            modifier = Modifier,
+            shape = RoundedCornerShape(12.dp),
+            colors = CardDefaults.cardColors(
+                containerColor = MaterialTheme.colorScheme.secondaryContainer,
+                contentColor = MaterialTheme.colorScheme.onSecondaryContainer,
+                disabledContainerColor = MaterialTheme.colorScheme.tertiaryContainer,
+                disabledContentColor = MaterialTheme.colorScheme.onTertiaryContainer
+            )
+        ) {
+            Text(
+                modifier = Modifier.padding(8.dp),
+                text = "Title",
+                style = MaterialTheme.typography.headlineMedium
+            )
+
+            Text(
+                modifier = Modifier.padding(8.dp),
+                text = "From your recent favorites",
+                style = MaterialTheme.typography.bodySmall
+            )
+
+            Button(
+                modifier = Modifier.padding(8.dp),
+                onClick = {
+                    /* Do something! */
+                }
+            ) {
+                Text("Buy")
+            }
+        }
+    }
+}
