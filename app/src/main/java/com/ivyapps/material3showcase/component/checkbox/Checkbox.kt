8aaae87b31ede3ca7ce8fb5aa1f4cd7bac3a23fa
Nicole <nicole.georgievva@gmail.com>
2023-05-26 16:41:20 +0300

    Change Checkbox image

--- app/src/main/java/com/ivyapps/material3showcase/component/checkbox/Checkbox.kt
+++ app/src/main/java/com/ivyapps/material3showcase/component/checkbox/Checkbox.kt
@@ -1,5 +1,8 @@
 package com.ivyapps.material3showcase.component.checkbox
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.CheckboxDefaults
 import androidx.compose.material3.MaterialTheme
@@ -7,10 +10,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.airbnb.android.showkase.annotation.ShowkaseComposable
 import com.ivyapps.material3showcase.component.ComponentPreview
 import com.ivyapps.material3showcase.component.ShowcasePreview
-import com.ivyapps.material3showcase.component.showkaseHeight
 import com.ivyapps.material3showcase.component.showkaseWidth
 
 @ComponentPreview
@@ -18,17 +21,26 @@ import com.ivyapps.material3showcase.component.showkaseWidth
     name = "Default",
     group = "Checkbox",
     widthDp = showkaseWidth,
-    heightDp = showkaseHeight,
+    heightDp = 100
 )
 @Composable
 fun CheckboxDefault() {
-    ShowcasePreview {
+    ShowcasePreview(height = 100) {
         val checkedState = remember { mutableStateOf(true) }
 
-        Checkbox(
-            checked = checkedState.value,
-            onCheckedChange = { checkedState.value = it }
-        )
+        Column {
+            Checkbox(
+                checked = !checkedState.value,
+                onCheckedChange = { checkedState.value = it }
+            )
+
+            Spacer(modifier = Modifier.height(8.dp))
+
+            Checkbox(
+                checked = checkedState.value,
+                onCheckedChange = { checkedState.value = it }
+            )
+        }
     }
 }
 
@@ -37,11 +49,11 @@ fun CheckboxDefault() {
     name = "Custom",
     group = "Checkbox",
     widthDp = showkaseWidth,
-    heightDp = showkaseHeight,
+    heightDp = 100
 )
 @Composable
 fun CheckboxCustom() {
-    ShowcasePreview {
+    ShowcasePreview(height = 100) {
         val checkedState = remember { mutableStateOf(true) }
 
         Checkbox(
