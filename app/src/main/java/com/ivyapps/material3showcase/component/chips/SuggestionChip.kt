3e68100adde8564c845bbb74668603ea1de069df
Nicole <nicole.georgievva@gmail.com>
2023-05-26 16:35:19 +0300

    Add Suggestion Chip

---
+++ app/src/main/java/com/ivyapps/material3showcase/component/chips/SuggestionChip.kt
@@ -0,0 +1,52 @@
+package com.ivyapps.material3showcase.component.chips
+
+import androidx.compose.material3.SuggestionChip
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import com.airbnb.android.showkase.annotation.ShowkaseComposable
+import com.ivyapps.material3showcase.component.ComponentPreview
+import com.ivyapps.material3showcase.component.ShowcasePreview
+import com.ivyapps.material3showcase.component.showkaseHeight
+import com.ivyapps.material3showcase.component.showkaseWidth
+
+@ComponentPreview
+@ShowkaseComposable(
+    name = "Default",
+    group = "Suggestion Chip",
+    widthDp = showkaseWidth,
+    heightDp = showkaseHeight
+)
+@Composable
+fun SuggestionChipDefault() {
+    ShowcasePreview() {
+        SuggestionChip(
+            onClick = {
+                /* Do something! */
+            },
+            label = {
+                Text("Thank you")
+            }
+        )
+    }
+}
+
+@ComponentPreview
+@ShowkaseComposable(
+    name = "Custom",
+    group = "Suggestion Chip",
+    widthDp = showkaseWidth,
+    heightDp = showkaseHeight,
+)
+@Composable
+fun SuggestionChipCustom() {
+    ShowcasePreview() {
+        SuggestionChip(
+            onClick = {
+                /* Do something! */
+            },
+            label = {
+                Text("Thank you")
+            }
+        )
+    }
+}
